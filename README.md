40732cba981909f0f7d896f7411dcc5cc975b524
Mike Chester <mike@chester.io>
2023-01-22 10:29:35 -0800

    feat: move path to url parsing so can pass in secrets easily

--- README.md
+++ README.md
@@ -3,10 +3,12 @@ Proxy for AlertManager heartbeat and Uptime Kuma.
 
 This is a small binary that essentially converts a `HTTP POST` into an `HTTP GET` since uptime-kuma push requests only support `HTTP GET`...
 
+Currently in use on my [k8s cluster](https://github.com/mchestr/home-cluster/blob/main/kubernetes/apps/monitoring/kube-prometheus-stack/app/helm-release.yaml#L121-L130)
+
 ### Usage
 
 ```
-export UPTIME_KUMA_URL="https://some.example.com/api/push/sometoken"
+export UPTIME_KUMA_URL="https://some.example.com"
 docker run -p 3000:3000 alertmanager-uptime-kuma-proxy
-curl -X POST http://localhost:3000
+curl -X POST http://localhost:3000/api/push/sometoken
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
-//! Run with
-//!
-//! ```not_rust
-//! cd examples && cargo run -p example-hello-world
-//! ```
+use axum::{
+    extract::{Path, State},
+    routing::{get, post},
+    Router,
+};
+use std::{env, net::SocketAddr, sync::Arc};
 
-use axum::{routing::post, Router};
-use std::net::SocketAddr;
-use std::env;
+struct AppState {
+    uptime_kuma_base_url: url::Url,
+}
 
 #[tokio::main]
 async fn main() {
+    let shared_state = Arc::new(AppState {
+        uptime_kuma_base_url: url::Url::parse(&env::var("UPTIME_KUMA_BASE_URL").unwrap()).unwrap(),
+    });
     // build our application with a route
-    let app = Router::new().route("/", post(handler));
+    let app = Router::new()
+        .route("/health", get(health))
+        .route("/*path", post(default_route))
+        .with_state(shared_state);
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -22,9 +29,11 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> String {
-    reqwest::get(env::var("UPTIME_KUMA_URL").unwrap())
-        .await.unwrap()
-        .text()
-        .await.unwrap()
+async fn health() -> String {
+    "{\"status\":\"OK\"}".into()
+}
+
+async fn default_route(State(state): State<Arc<AppState>>, Path(path): Path<String>) -> String {
+    let url = state.uptime_kuma_base_url.join(&path.to_string()).unwrap();
+    reqwest::get(url).await.unwrap().text().await.unwrap()
 }
